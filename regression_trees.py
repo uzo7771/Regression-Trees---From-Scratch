f871ab668b74a32cbbd77bb0e341a675bf58c0c5
Patryk Kaplan <machbreiven@gmail.com>
2025-01-26 12:07:54 +0100

    add n_features option

--- regression_trees.py
+++ regression_trees.py
@@ -1,22 +1,22 @@
 import numpy as np
+import random
 
 class TreeNode:
     def __init__(self, feature: int = None, threshold: float = None, left: 'TreeNode' = None, 
-                 right: 'TreeNode' = None, value: int = None, cost: float = None, num_leaves: int = 0,
-                 y_indices: np.array = None):
+                 right: 'TreeNode' = None, value: int = None, cost: float = None):
         self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
         self.value = value
         self.cost = cost
-        self.num_leaves = num_leaves
-        self.y_indices = y_indices
 
 class RegressionTree:
-    def __init__(self, X, y, max_depth=10, min_samples_split=5, min_samples_leaf = 1):
+    def __init__(self, X, y, max_depth=10, min_samples_split=5, min_samples_leaf = 1, n_features=None):
         if len(X.shape) == 1:
             self.X = X = X.reshape(-1, 1)
+        if n_features > np.shape(X)[1]:
+            raise ValueError(f"The maximum number of features is {np.shape(X)[1]}")
         self.X = X
         self.y = y
         self.max_depth = max_depth
@@ -24,6 +24,7 @@ class RegressionTree:
         self.root = None
         self.min_samples_leaf = min_samples_leaf
         self.feature_importance = np.zeros(self.X.shape[1])   
+        self.n_features = n_features
     def _MSE(self, y):
         MSE = np.mean((y - np.mean(y))**2)
         return MSE
@@ -47,9 +48,15 @@ class RegressionTree:
         return cost_left, cost_right 
     def _find_best_split(self, X, y, H):
         # Finds the best feature and threshold to split the data.
+        if self.n_features is not None:
+            possible_features_indexes = random.sample(range(0,np.shape(X)[1]), self.n_features)
+        else:
+            possible_features_indexes = range(np.shape(X)[1])
+
         best_feature, best_threshold, best_variance_reduction = None, None, -np.inf
         best_cost_left, best_cost_right = None, None
-        for feature in range(X.shape[1]):
+
+        for feature in possible_features_indexes:
             tmp = np.sort(np.unique(X[:,feature]))
            #possible_thresholds = [np.mean(tmp[i:i+2]) for i in range(len(tmp)-1)]
             possible_thresholds = np.histogram_bin_edges(tmp, bins=20)[1:-1]
@@ -65,42 +72,36 @@ class RegressionTree:
                 "cost_left": best_cost_left, "cost_right": best_cost_right}
     def _build_tree(self, X, y, cost, depth=0):
          # Recursively builds the decision tree.
-        y_indices = np.isin(self.y, y)
         if len(np.unique(y)) == 1:
-            self.a +=1
-            return TreeNode(value=np.unique(y)[0], cost = cost, y_indices=y_indices)
+            return TreeNode(value=np.unique(y)[0], cost = cost)
         
         if depth >= self.max_depth or len(y) <= self.min_samples_split:
-            self.a +=1
-            return TreeNode(value=np.mean(y), cost = cost, y_indices=y_indices)
+            return TreeNode(value=np.mean(y), cost = cost)
         
+
         best_feature, best_threshold, best_variance_reduction, cost_left, cost_right = self._find_best_split(X, y, cost).values()
 
-        if best_variance_reduction == -np.inf or best_variance_reduction == np.inf:
-            self.a +=1
-            return TreeNode(value=np.mean(y), cost = cost, y_indices=y_indices) 
-        else:
-            self.feature_importance[best_feature] += best_variance_reduction / (depth + 1)
+        if best_variance_reduction == -np.inf or best_variance_reduction == np.inf or best_variance_reduction < 0:
+            return TreeNode(value=np.mean(y), cost = cost) 
         
+        
+        self.feature_importance[best_feature] += best_variance_reduction / (depth + 1)
         
         left_mask = X[:, best_feature] <= best_threshold
         right_mask = ~left_mask
 
         if len(y[left_mask]) < self.min_samples_leaf or len(y[right_mask]) < self.min_samples_leaf:
-            self.a +=1
             return TreeNode(value=np.mean(y), cost = cost)
 
         left_node = self._build_tree(X = X[left_mask], y = y[left_mask], depth = depth + 1, cost = cost_left)
         right_node = self._build_tree(X = X[right_mask], y = y[right_mask], depth = depth + 1, cost = cost_right)
 
-        return TreeNode(best_feature, best_threshold, left_node, right_node, cost = cost, y_indices=y_indices)
+        return TreeNode(best_feature, best_threshold, left_node, right_node, cost = cost)
     def fit(self):
         # Trains the decision tree.
-        self.a = 0
         self.root = self._build_tree(X = self.X, y = self.y, cost = self._MSE(self.y))
         total_importance = np.sum(self.feature_importance)
         if total_importance > 0:
-            print(self.feature_importance, total_importance)
             self.feature_importance /= total_importance
     def _predict(self, X):
          # Predicts a single sample.
